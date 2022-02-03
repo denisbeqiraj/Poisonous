76e95300f4be6a2eefa8e8548b31fa2cbe7c9cc2
Denis Beqiraj <denisbeqi2312@gmail.com>
2022-02-03 22:42:56 +0100

    Added player

---
+++ Assets/Scripts/JoinBoss.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class JoinBoss : MonoBehaviour
+{
+    public GameObject stats;
+    private SharedStats shared_stats;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shared_stats = stats.GetComponent<SharedStats>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Magnitude(player.transform.position - gameObject.transform.position) < 10) {
+            string difficulty = shared_stats.getDifficulty();
+            switch (difficulty)
+            {
+                case "EASY":
+                    SceneManager.LoadScene("EasyBossScene");
+                    break;
+
+                case "HARD":
+                    SceneManager.LoadScene("HardBossScene");
+                    break;
+            }
+        }
+    }
+}
